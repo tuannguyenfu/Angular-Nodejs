ade7d541540a0b43fe6f0a30695b90d177f0bc3c
tuannguyen <tuannguyenfu@gmail.com>
2023-05-07 14:02:32 +0700

    Initial new BE service

---
+++ avaj-service/avaj/src/main/java/com/tuannguyenfu/avaj/AvaJProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tuannguyenfu.avaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaJProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaJProjectApplication.class, args);
+	}
+
+}
---
+++ avaj-service/avaj/src/test/java/com/tuannguyenfu/avaj/AvaJProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuannguyenfu.avaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaJProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
